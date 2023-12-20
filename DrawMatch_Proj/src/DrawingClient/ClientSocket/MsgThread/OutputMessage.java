5a747ac17996d268ffb94142e08bf4a9603edb6c
rtgc0915 <rtgc0915@gmail.com>
2023-12-20 12:46:26 +0900

    Update OutputMessage.java

--- DrawMatch_Proj/src/DrawingClient/ClientSocket/MsgThread/OutputMessage.java
+++ DrawMatch_Proj/src/DrawingClient/ClientSocket/MsgThread/OutputMessage.java
@@ -5,14 +5,14 @@ import java.io.PrintWriter;
 import java.net.Socket;
 //서버로 메시지를 보내는 스레드 클래스
 public class OutputMessage extends Thread{
+	private String id;
 	private Socket Server;
 	static public PrintWriter msg;
-	private String id;
 	
 	public void run() {
 		super.run();
-		createSender();
-		sendId();
+		CreateOutputMessage();
+		OutputId();
 	}
 	
 	public void setId(String id) {
@@ -21,14 +21,14 @@ public class OutputMessage extends Thread{
 	public void setSocket(Socket Server) {
 		this.Server = Server;
 	}
-	private void createSender() {
+	private void CreateOutputMessage() {
 		try {
 			msg = new PrintWriter(Server.getOutputStream());
 		}catch(IOException e) {
-			e.printStackTrace();
+			System.out.println("[오류] 발생");
 		}
 	}
-	private void sendId() {
+	private void OutputId() {
 		msg.println(id);
 		msg.flush();
 	}
